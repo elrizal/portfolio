5711fad5b1235bf3b0782ea6f47a2a9c89c04537
elrizal <erin@lrizal.com>
2019-11-07 23:48:30 -0500

    grid exp

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,14 @@ import { Parallax } from 'react-parallax';
 import ScrollAnimation from 'react-animate-on-scroll';
 import '../css/header.sass';
 import Nav from '../components/nav';
-import Zoom from 'react-reveal/Zoom';
 
 const backGround = 'https://res.cloudinary.com/diuubtvqd/image/upload/v1572981281/bottom-marble.jpg'
-
 
 const Home = () => (
   <div className="home-section">
     <Nav/>
     <div>
-    <Parallax className="top" bgImage={backGround} strength={400}>
+    <Parallax className="top" bgImage={backGround} strength={300}>
       <div style={{ height: 700 }}>
         <div className="titlecontain">
           <h1 className='header-title t-shadow'>Erin Leigh Rizal</h1>
@@ -21,53 +19,16 @@ const Home = () => (
         </div>
       </div>
     </Parallax>
-    <div class="tile is-ancestor">
-  <div class="tile is-vertical is-8">
-    <div class="tile">
-      <div class="tile is-parent is-vertical">
-        <article class="tile is-child notification is-primary">
-          <p class="title">Vertical...</p>
-          <p class="subtitle">Top tile</p>
-        </article>
-        <article class="tile is-child notification is-warning">
-          <p class="title">...tiles</p>
-          <p class="subtitle">Bottom tile</p>
-        </article>
-      </div>
-      <div class="tile is-parent">
- 
-      </div>
-    </div>
-    <div class="tile is-parent">
-      <article class="tile is-child notification is-danger">
-        <p class="title">Wide tile</p>
-        <p class="subtitle">Aligned with the right tile</p>
-        <div class="content">
-        </div>
-      </article>
-    </div>
-  </div>
-  <div class="tile is-parent">
-    <article class="tile is-child notification is-success">
-      <div class="content">
-        <p class="title">Tall tile</p>
-        <p class="subtitle">With even more content</p>
-        <div class="content">
-         
-        </div>
-      </div>
-    </article>
-  </div>
-</div>
-    <section class="section">
-    <div class="container">
-      <h1 class="title">Section</h1>
-      <h2 class="subtitle">
+
+    <section className="section">
+    <div className="container">
+      <h1 className="title">Section</h1>
+      <h2 className="subtitle">
         A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
       </h2>
     </div>
   </section>
-    <div class="container">
+    <div className="container is-fluid">
     <p>Ad pariatur eiusmod nulla ex proident nulla anim amet ipsum aliqua anim ipsum. 
       Proident irure adipisicing enim aliqua ea sint ut reprehenderit elit nulla anim excepteur veniam excepteur. 
       Ut anim consectetur nostrud Lorem aliquip est laborum deserunt ut anim enim magna. 
@@ -78,7 +39,24 @@ const Home = () => (
     </div>
     </div>
     <ScrollAnimation animateIn='fadeIn'>
-        <div class="container is-fluid" ><h2>Showcase:</h2>
+        <div className="s" >
+          <h2>Showcase:</h2>
+        <div className="columns is-mobile">
+          <div className="column casegal web">
+            Web Development 
+          </div>
+          <div className="column casegal" id="ux">
+            UX-UI Design Case Studies
+          </div>
+          <div className="column casegal ad">
+            Art Direction
+          </div>
+          <div className="column casegal brandd">
+            Brand Design
+          </div>
+        </div>
+        Mics. Case Studies ...
+
         <p> Here are sections with samples of work I have done for my clients and employers from 2011-2019 - </p></div>
         <div align='center' id='introgridc'>
           <a className='introgrid' href="/uxui">
@@ -91,9 +69,6 @@ const Home = () => (
           <img className='introgrid' src={'https://res.cloudinary.com/diuubtvqd/image/upload/v1555423791/work/social.jpg'} alt='social media'/> </a>
         </div>  
         </ScrollAnimation>
-        <Zoom>
-  <p>Markup that will be revealed on scroll</p>
-</Zoom>
 
   </div>
 )
