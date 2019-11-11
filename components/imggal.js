e7969ccaae716e11bdf791849ca0326961dd67e0
elrizal <erin@lrizal.com>
2019-11-10 23:44:01 -0500

    reorganaization

---
+++ components/imggal.js
@@ -0,0 +1,17 @@
+
+import '../css/styles.sass';
+
+const ImgGal = () => {
+  const toggleStyles = (event) => {
+    document.querySelector('#burger').classList.toggle('is-active')
+    document.querySelector('#navbarmenu').classList.toggle('is-active')
+  }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ImgGal;
---
+++ data/gallerylist.js
@@ -0,0 +1,10 @@
+const test = [{
+    url: 'http',
+    category: 'ux',
+    toolsused: 'adobe ps'
+},
+{
+    url: 'http',
+    category: 'web',
+    toolsused: 'react'
+}]
