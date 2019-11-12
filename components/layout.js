3dfd890975d2747c10e3343b1dabee8f8424fd01
elrizal <erin@lrizal.com>
2019-11-11 23:08:43 -0500

    nav

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import '../css/styles.sass';
-
+import Nav from './nav';
 export default ({ children }) => {
   /*
-   * Added this to toggle the is-active class. See:
+   * Added this to toggle the is-active className. See:
    * 
    * https://bulma.io/documentation/components/navbar/#navbar-menu
    * https://github.com/jgthms/bulma/issues/856
@@ -21,38 +21,7 @@ export default ({ children }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"></link>
       </Head>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-          <div className="navbar-brand graybg">
-            <a className="navbar-item">
-
-            </a>
-            <a id="burger" onClick={toggleStyles} 
-                role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarmenu">
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
-          </div>
-          <div id="navbarmenu" className="navbar-menu">
-            <div className="navbar-start">
-              <Link  href="/">
-                <a className="navbar-item">Home</a>
-              </Link>
-              <Link href='/post/[id]' as='/post/first'>
-                <a className="navbar-item">First</a>
-              </Link>
-              <Link href='/post/[id]' as='/post/second'>
-                <a className="navbar-item">Second</a>
-              </Link>
-            </div>
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <div className="buttons">
--                </div>
-              </div>
-            </div>
-          </div>
-        </nav>
+      <Nav/>
         <div className="content">
         {children}
         </div>
--- components/nav.js
+++ components/nav.js
@@ -18,34 +18,60 @@ const toggleStyles = (event) => {
 }
 
 const Nav = () => (
-  <nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand graybg">
-    <a className="navbar-item">
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+ELR
 
-    </a>
-    <a id="burger" onClick={toggleStyles} 
-        role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarmenu">
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-  <div id="navbarmenu" className="navbar-menu">
-    <div className="navbar-start">
-      <Link  href="/">
-        <a className="navbar-item">Home</a>
-      </Link>
-      <Link href='/post/[id]' as='/post/first'>
-        <a className="navbar-item">First</a>
-      </Link>
-      <Link href='/post/[id]' as='/post/second'>
-        <a className="navbar-item">Second</a>
-      </Link>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider"/>
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
     </div>
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="buttons">
--                </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
       </div>
     </div>
   </div>
