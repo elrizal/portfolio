d74258e6a04e31750d56a4088ece4d1f8fc9ca37
elrizal <erin@lrizal.com>
2020-02-01 20:21:24 -0500

    art direction cases being added

--- pages/artdir/[itemselected].jsx
+++ pages/artdir/[itemselected].jsx
@@ -1,26 +1,28 @@
-[
-    {
-      "id": 1,
-      "order": "fhproduct",
-      "title": "2016 Line Product Photography",
-      "type": "Digital & Print",
-      "timeframe": "April - September 2016",
-      "image": "",
-      "tools": [
-        "Lightroom",
-        "HTML5",
-        "CSS",
-        "MySQL"
-      ],
-      "libapi": [
-        "Node JS", "Express", "jQuery", "Google Maps", "Google Books", "Passport JS"
-      ],
-      "role": "full stack development",
-      "description": "This social app is targeted for users seeking to exchange books in their local area. The user simply signs up, records the books they own, and can seek out other users looking for books. ",
-      "problem":"The idea came from observing the lack of connection in online book-lover forums. Although there is a lot of avenues of selling books online/in person, my co-developers and I observed that there’s more opportunity to expand beyond those current options. Rather than focus on the transactional purpose of sending off your books, there was a lack of connection in a similar manner to a local book club or the “little free library”.  Therefore, we were interested in incorporating the more personable aspect of it such as libraries, book stores etc. that seem to have slipped by the wayside.  ",
-      "process":"In our team of 4 developers, we all divided tasks based on priority since we were balanced in terms of strengths in front and back end development. After a condensed “design sprint” of compared similar existing websites and wireframing as a group, we decided on the stack we were using quickly. Given our tight turnaround, I used jQuery for quick DOM manipulation and a mixture of custom CSS with the library, Materialize. Once we had a prototype of working interactions, we agreed on using MySQL for the need of relational data; since interactions between users would be based on comparing data (such as checking if a book has been “checked out”), it was the most fitting at the time. By choosing MySQL, also were able to take advantage of Passport JS for easy control of the authentication process.  Once we planned out what we needed for the back end, we connected the Google Books API with “dummy” user accounts to produce the basis of the experience.",
-      "future":"As a result, we had a functioning MVP of our initial idea. Users can sign up and browse books, while being able to view who is available in their local area. For future development, we are seeking to build and integrate a chat, rather than falling back of emails between users. ",
-      "github": "https://github.com/elrizal/its-lit",
-      "live": "http://sleepy-peak-84439.herokuapp.com/"
-    }
-  ]
+import galleryList from './items-campaigns.json';
+import Layout from '../../../components/layout'
+import React from 'react';
+import {useRouter} from 'next/router'
+import Link from 'next/link';
+import ArrowLeftRounded from '@material-ui/icons/ArrowLeftRounded';
+import Button from '@material-ui/core/Button';
+
+const Comment = () => {
+  const router = useRouter();
+  const {id, itemselected} = router.query;
+  return <Layout>
+      <div className="fixed backbtn">
+        <Link href='/campaign/[id]' as='/campaign/index'>
+          <a>
+            <ArrowLeftRounded style={{
+              fontSize: 90
+            }}/>
+          </a>
+        </Link>
+      </div>
+      <div className="container ">
+
+      </div>
+    </Layout>
+}
+
+export default Comment
