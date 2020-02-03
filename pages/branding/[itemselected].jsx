cdba5513ee57d815e8a1ebef87a9054cea8326f0
elrizal <erin@lrizal.com>
2020-02-02 23:46:51 -0500

    branding

--- pages/branding/[itemselected].jsx
+++ pages/branding/[itemselected].jsx
@@ -1,6 +1,6 @@
 import galleryList from './items-campaigns.json';
+import Layout from '../../../components/layout'
 import React from 'react';
-import Layout from '../../../components/layout';
 import {useRouter} from 'next/router'
 import Link from 'next/link';
 import ArrowLeftRounded from '@material-ui/icons/ArrowLeftRounded';
@@ -10,18 +10,7 @@ const Comment = () => {
   const router = useRouter();
   const {id, itemselected} = router.query;
   return <Layout>
-      <div className="fixed backbtn">
-        <Link href='/campaign/[id]' as='/campaign/index'>
-          <a>
-            <ArrowLeftRounded style={{
-              fontSize: 90
-            }}/>
-          </a>
-        </Link>
-      </div>
-      <div className="container ">
-
-      </div>
+   
     </Layout>
 }
 
