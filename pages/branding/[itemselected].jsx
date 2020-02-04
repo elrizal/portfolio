0d50ccafc169b225cfa6b20118b1a699337d0ffa
elrizal <erin@lrizal.com>
2020-02-03 23:23:51 -0500

    debugging small syntax errors for deployment

--- pages/branding/[itemselected].jsx
+++ pages/branding/[itemselected].jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import {useRouter} from 'next/router';
-import Link from 'next/link';
-import ArrowLeftRounded from '@material-ui/icons/ArrowLeftRounded';
-import Button from '@material-ui/core/Button';
 
 const Comment = () => {
   const router = useRouter();
