673431b2b409af60bd29642fb41eaa985e7f2fb4
elrizal <erin@lrizal.com>
2020-02-03 23:05:02 -0500

    adding more cs categories

--- pages/branding/index.jsx
+++ pages/branding/index.jsx
@@ -1,6 +1,6 @@
 import {useRouter} from 'next/router';
 import Link from 'next/link';
-import Layout from '../../../components/layout';
+import Layout from '../../components/layout';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
@@ -12,38 +12,38 @@ const Branding = () => {
   return (
     <Layout>
       <div className="container center animated fadeIn">
-      <h2>Campaigns</h2>
-      <p>Case studies of online and out-of-home campaigns where I was a lead visual designer:</p>
+      <h2>Branding</h2>
+      <p>Cases where I led the visual direction for identity design and online/Out-of-home brand voice</p>
         <div className="row">
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/first-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/studentlife-loyola`}>
               <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576719479/greycominterest.jpg" className="case-thumb" alt="greycomm study"/></a>
             </Link>
             <p><b>New Hiring at Greycomm Studios</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/first-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/studentlife-loyola`}>
               <a>Case Study</a>
             </Link>
             </Button>
           </div>
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/second-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/cog-systems`}>
             <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576719478/wc.jpg" className="case-thumb"  alt="women of color conference"/></a>
             </Link>
             <p><b>Promotion for Women of Color Conference</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/second-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/cog-systems`}>
               <a>Case Study</a>
             </Link>
             </Button>
           </div>
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/third-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/sheas`}>
             <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576766036/work/th-ccsjspring.jpg" className="case-thumb"  alt="women of color conference"/></a>
             </Link>
             <p><b>Ad for Loyola's Community Service Center</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/third-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/sheas`}>
               <a>Case Study</a>
             </Link>
             </Button>
@@ -52,34 +52,34 @@ const Branding = () => {
 
         <div className="row">
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/fourth-item`}>
-              <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576771531/work/ooh/fhfal.jpg" className="case-thumb" alt="FH holiday email study"/></a>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/fh-products`}>
+              <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576719479/greycominterest.jpg" className="case-thumb" alt="greycomm study"/></a>
             </Link>
-            <p><b>Pre-holiday sales promotion at FH Group Int'l</b></p>
+            <p><b>New Hiring at Greycomm Studios</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/fourth-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/fh-products`}>
               <a>Case Study</a>
             </Link>
             </Button>
           </div>
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/fifth-item`}>
-            <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576771873/work/ooh/fhchr.jpg" className="case-thumb"  alt="FH sales event"/></a>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/dresday`}>
+            <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576719478/wc.jpg" className="case-thumb"  alt="women of color conference"/></a>
             </Link>
-            <p><b>Holiday sales event at FH Group Int'l</b></p>
+            <p><b>Promotion for Women of Color Conference</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/fifth-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/dresday`}>
               <a>Case Study</a>
             </Link>
             </Button>
           </div>
           <div className="col-md-6 col-lg-4 case-contain">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/sixth-item`}>
-            <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576772849/work/ooh/bombas.jpg" className="case-thumb"  alt="women of color conference"/></a>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/greycomm`}>
+            <a><img src="https://res.cloudinary.com/diuubtvqd/image/upload/v1576766036/work/th-ccsjspring.jpg" className="case-thumb"  alt="women of color conference"/></a>
             </Link>
-            <p><b>Bombas holiday promotions</b></p>
+            <p><b>Ad for Loyola's Community Service Center</b></p>
             <Button variant="outlined">
-            <Link href="/campaign/[id]/[itemselected]" as={`/campaign/${id}/sixth-item`}>
+            <Link href="/branding/[id]/[itemselected]" as={`/branding/${id}/greycomm`}>
               <a>Case Study</a>
             </Link>
             </Button>
