510c472c153baa0e200ee4f627d690ca0dcde5de
elrizal <erin@lrizal.com>
2020-02-03 23:18:58 -0500

    adding more cs categories

--- pages/branding/[itemselected].jsx
+++ pages/branding/[itemselected].jsx
@@ -1,5 +1,5 @@
 import galleryList from './items-campaigns.json';
-import Layout from '../../../components/layout';
+
 import React from 'react';
 import {useRouter} from 'next/router';
 import Link from 'next/link';
@@ -9,9 +9,9 @@ import Button from '@material-ui/core/Button';
 const Comment = () => {
   const router = useRouter();
   const {id, itemselected} = router.query;
-  return <Layout>
+  return <div>
    
-    </Layout>
+    </div>
 }
 
 export default Comment
