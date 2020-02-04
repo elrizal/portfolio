5f003a8a64c795589338d35f132c22d569d27b6e
elrizal <erin@lrizal.com>
2020-02-03 23:26:30 -0500

    debugging small syntax errors for deployment

---
+++ pages/print/[itemselected].jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useRouter} from 'next/router';
+
+const Comment = () => {
+  const router = useRouter();
+  const {id, itemselected} = router.query;
+  return <div>
+   
+    </div>
+}
+
+export default Comment
