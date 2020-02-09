5cfbcdc9e2c509493d318b01dddcb97d88af2454
elrizal <erin@lrizal.com>
2020-02-08 19:11:32 -0500

    printsetup

--- pages/print/[itemselected].jsx
+++ pages/print/[itemselected].jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {useRouter} from 'next/router';
+import Layout from '../../../components/layout';
 
 const Comment = () => {
   const router = useRouter();
   const {id, itemselected} = router.query;
-  return <div>
-   
-    </div>
+  return <Layout>
+  <div className ="container">
+    <h1>Print Design</h1>
+  </div>
+   </Layout>
 }
 
 export default Comment
