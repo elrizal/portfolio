649779bd56073219c347d7b5420d905b843392f3
elrizal <erin@lrizal.com>
2020-02-24 23:33:43 -0500

    brand case studies

--- pages/web/[id]/[itemselected].js
+++ pages/web/[id]/[itemselected].js
@@ -4,6 +4,7 @@ import React from 'react';
 import {useRouter} from 'next/router';
 import Button from '@material-ui/core/Button';
 import BackBtn from '../../../components/back';
+
 const Comment = () => {
   const router = useRouter();
   const {id, itemselected} = router.query;
