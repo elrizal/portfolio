3623084439d3370777f020f48876f5fe70c95a68
elrizal <erin@lrizal.com>
2022-03-13 23:58:27 -0500

    updating

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const withSass = require('@zeit/next-sass')
-module.exports = {
+const withSass = require('@zeit/next-sass');
+const nextConfig  = {
   target: 'serverless',
   ...withSass(),
   cssModules: true,
 }
-
+// next.config.js 
+// module.exports = withPlugins([...], {
+//   webpack5: false,
+// })
 // next.config.js
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS(withSass());
+module.exports = nextConfig;
