93a13b471145da78a90a0f43ef95f57aacae4a56
elrizal <erin@lrizal.com>
2022-08-01 13:53:08 -0500

    package change

--- next.config.js
+++ next.config.js
@@ -4,19 +4,9 @@ const withCSS = require('@zeit/next-css')
 module.exports = async (phase, {
   defaultConfig
 }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
   const nextConfig = {
       target: 'serverless',
   }
   return nextConfig
 }
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-}
-
 module.exports = nextConfig
