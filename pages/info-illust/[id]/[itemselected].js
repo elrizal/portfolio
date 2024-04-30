21e2e093896df9bf9b6313bdee21455782a120c6
Erin Rizal <erin@lrizal.com>
2024-04-29 22:58:08 -0500

    s

--- pages/info-illust/[id]/[itemselected].js
+++ pages/info-illust/[id]/[itemselected].js
@@ -1,53 +1,66 @@
-import galleryList from './cases.json';
-import React from 'react';
-import {useRouter} from 'next/router'
-import Layout from '../../../components/layout';
-import BackBtn from '../../../components/back';
+import galleryList from "./cases.json";
+import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../../../components/layout";
+import BackBtn from "../../../components/back";
 const Comment = () => {
   const router = useRouter();
-  const {id, itemselected} = router.query;
+  const { id, itemselected } = router.query;
 
-  return  <Layout>
-      <BackBtn/>
+  return (
+    <Layout>
+      <BackBtn />
       <div className="container">
-        {galleryList.map(obj => {
+        {galleryList.map((obj) => {
           if (itemselected == obj.endpoint) {
-            return <div>
-              <div className="row animated fadeIn">
-                <div className="col-sm-12  col-lg-6">
-                <a href={obj.img} target="_blank">
-                <img src={obj.img} alt="Case" className='aside-caseimg  add-centering'/>
-                </a>
-                
-                {obj
-                  .additionalimgs
-                  .map(objImg => {
-                    return <div className="col-sm-6 col-md-6 col-lg-6">
-                      <a href={objImg} target="_blank">
-                        <img src={objImg} alt={obj.endpoint} className="added-imgs"/>
-                      </a>
-                    </div>
-                  })}
-                </div>
-                <div className="col-sm-12  col-lg-6">
-                <h1>{obj.title}</h1>
-                <h2>{obj.company}</h2>
-                  <p>
-                    <b>Responsibilities:</b> {obj.role}<br/>
-                    <b>Project timeframe: </b>
-                    {obj.timeframe}
-                  </p>
-                  <h3>About the project</h3>
-                  <p>{obj.description}</p>
-                  <h3>The process</h3>
-                  <p>{obj.process}</p>
+            return (
+              <div>
+                <div className="row animated fadeIn">
+                  <div className="col-sm-12  col-lg-6">
+                    <a href={obj.img} target="_blank">
+                      <img
+                        src={obj.img}
+                        alt="Case"
+                        className="aside-caseimg  add-centering"
+                      />
+                    </a>
+
+                    {obj.additionalimgs.map((objImg) => {
+                      return (
+                        <div className="col-sm-6 col-md-6 col-lg-6">
+                          <a href={objImg} target="_blank">
+                            <img
+                              src={objImg}
+                              alt={obj.endpoint}
+                              className="added-imgs"
+                            />
+                          </a>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="col-sm-12  col-lg-6">
+                    <h1>{obj.title}</h1>
+                    <h2>{obj.company}</h2>
+                    <p>
+                      <b>Responsibilities:</b> {obj.role}
+                      <br />
+                      <b>Project timeframe: </b>
+                      {obj.timeframe}
+                    </p>
+                    <h3>About the project</h3>
+                    <p>{obj.description}</p>
+                    <h3>The process</h3>
+                    <p>{obj.process}</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            );
           }
         })}
       </div>
     </Layout>
-}
+  );
+};
 
-export default Comment
+export default Comment;
